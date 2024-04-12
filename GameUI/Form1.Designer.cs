fea7baf2c1f5361007f9615d65fe383cc11b46d7
whflf <fluffy.jesus@yandex.ru>
2024-04-12 09:27:55 +0300

    add repainting when the window is resized

--- GameUI/Form1.Designer.cs
+++ GameUI/Form1.Designer.cs
@@ -28,12 +28,64 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            Control = new Panel();
+            tableLayoutPanel1 = new TableLayoutPanel();
+            tableLayoutPanel1.CellBorderStyle = TableLayoutPanelCellBorderStyle.None;
+            panel1 = new Panel();
+            tableLayoutPanel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // Control
+            // 
+            Control.BackColor = SystemColors.ActiveCaption;
+            Control.Dock = DockStyle.Fill;
+            Control.Location = new Point(3, 3);
+            Control.Name = "Control";
+            Control.Size = new Size(794, 399);
+            Control.TabIndex = 0;
+            Control.Paint += ControlPaint;
+            Control.Resize += ControlResize;
+            // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.ColumnCount = 1;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100F));
+            tableLayoutPanel1.Controls.Add(Control, 0, 0);
+            tableLayoutPanel1.Controls.Add(panel1, 0, 1);
+            tableLayoutPanel1.Dock = DockStyle.Fill;
+            tableLayoutPanel1.Location = new Point(0, 0);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 2;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 90F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 10F));
+            tableLayoutPanel1.Size = new Size(800, 450);
+            tableLayoutPanel1.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            panel1.BackColor = SystemColors.ActiveCaption;
+            panel1.Dock = DockStyle.Fill;
+            panel1.Location = new Point(3, 408);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(794, 39);
+            panel1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(tableLayoutPanel1);
+            Name = "Form1";
+            Text = "Form1";
+            tableLayoutPanel1.ResumeLayout(false);
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private Panel Control;
+        private TableLayoutPanel tableLayoutPanel1;
+        private Panel panel1;
     }
 }
--- GameUI/Form1.cs
+++ GameUI/Form1.cs
@@ -1,3 +1,6 @@
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+
 namespace GameUI
 {
     public partial class Form1 : Form
@@ -5,6 +8,42 @@ namespace GameUI
         public Form1()
         {
             InitializeComponent();
+        }
+
+        //private void 
+
+        private void ControlPaint(object sender, PaintEventArgs e)
+        {
+            var map = new Map("map.txt");
+
+            var size =
+                Math.Min(Control.Size.Width / map.MapComponents[0].Length, Control.Size.Height / map.MapComponents.Length);
+
+            var currentCoordsHorizontal = new Point(0, 0);
+            var currentCoordsVertical = new Point(0, 0);
+
+            foreach (var c in map.MapComponents)
+            {
+                foreach (var c2 in c)
+                {
+                    if (c2 != ' ')
+                    {
+                        var rect = new Rectangle(currentCoordsHorizontal.X, currentCoordsHorizontal.Y, size, size);
+                        var brush = new SolidBrush(Color.Pink);
+                        e.Graphics.FillRectangle(brush, rect);
+                    }
+
+                    currentCoordsHorizontal.X += size;
+                }
+
+                currentCoordsVertical.Y += size;
+                currentCoordsHorizontal = new Point(0, currentCoordsVertical.Y);
+            }
+        }
+
+        private void ControlResize(object sender, EventArgs e)
+        {
+            Control.Invalidate();
         }
     }
 }
